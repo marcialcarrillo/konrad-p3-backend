82cd4327b26da6d3d2ed37dac4870e2162bb191f
Marcial Carillo <marcialcarrillov@gmail.com>
2022-06-27 11:16:05 -0600

    initial commit

--- src/config/sequelize.config.js
+++ src/config/sequelize.config.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const path = "mysql://root:admin@localhost:3306/users";
+const path = "mysql://root:admin@localhost:3306/bank";
 const sequelize = new Sequelize(path);
 
 // sequelize
